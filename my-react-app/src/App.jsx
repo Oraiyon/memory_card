c35249b208af031d50e68403f01f0ed6ab760bc6
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-01-03 14:54:53 +0800

    Created handleClick(). handleClick() uses shuffle() to randomly sort cards and increases currentScore per click. Also moved cards state to Cards.jsx.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,39 +6,27 @@ import Cards from './Cards.jsx';
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState([]);
+  const [pokemons, setPokemons] = useState(
+    ["charizard", "pikachu", "mew","rayquaza",
+    "snorlax", "lugia",  "mewtwo", "bulbasaur",
+    "gengar", "magikarp", "groudon", "zapdos",
+    "kyogre", "squirtle", "charmander", "geodude"]
+  );
   
-  const pokemons = ["charizard", "pikachu", "mew", "rayquaza", 
-                    "snorlax", "lugia",  "mewtwo", "bulbasaur",
-                    "gengar", "magikarp", "groudon", "zapdos",
-                    "kyogre", "squirtle", "charmander", "geodude"];
-
-  // React.StrictMode displays fetched cards twice
-  // Only in dev mode
-  useEffect(
-    () => {
-      pokemons.map(pokemon => {
-        const controller = new AbortController();
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-          .then(res => res.json())
-          .then(data => setCards(c => ([...c, data])))
-        return () => {
-          controller.abort();
-        }
-      });
-    },[]
-  )
-
   return (
     <>
       <h1>Pokemon Memory Game</h1>
       <header>
         <h2>Get points by clicking on a pokemon, but don't repeat a pick!</h2>
         <Scoreboard 
-            currentScore = {currentScore} 
-            bestScore = {bestScore}/>
+            currentScore={currentScore} 
+            bestScore={bestScore}/>
       </header>
-      <Cards cards = {cards}/>
+      <Cards 
+        pokemons={pokemons} 
+        setPokemons={setPokemons}
+        currentScore={currentScore}
+        setCurrentScore={setCurrentScore}/>
     </>
   );
 }
--- my-react-app/src/Cards.jsx
+++ my-react-app/src/Cards.jsx
@@ -1,16 +1,51 @@
 import "./Cards.css"
+import { useState, useEffect} from "react";
 
 const Cards = (props) => {
+    const [cards, setCards] = useState([]);
+    
+    useEffect(() => {
+        props.pokemons.map(pokemon => {
+            const controller = new AbortController();
+
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+                .then(res => res.json())
+                .then(data => setCards(c => ([...c, data])))
+
+            return () => {
+            controller.abort();
+            }
+        });
+    },[]);
+    
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
+    const handleClick = () => {
+        const shuffled = shuffle(cards);
+        setCards(c => (shuffled));
+        props.setCurrentScore(c => (c + 1))
+    }
+
     const capitalizeName = (name) => {
         const firstLetter = name.charAt(0).toUpperCase();
         const rest = name.slice(1)
         return firstLetter.concat(rest);
     }
 
+    // React.StrictMode displays fetched cards twice
+    // Only in dev mode
     return (
         <div className="cards">
-            {props.cards.map(card => 
-                <button className="card" key={card.id}>
+            {cards.map(card => 
+                <button className="card" 
+                key={card.id + "-" + Math.random(10)}
+                onClick={handleClick}>
                     <img src={card.sprites.front_default} 
                          alt="picture of pokemon" >
                     </img>
