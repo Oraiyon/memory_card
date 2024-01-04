1c836f6c6c1746be86b01aeb6986f6e4b0ac39bf
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-01-04 13:39:01 +0800

    Made prop elements bold

--- my-react-app/src/Scoreboard.jsx
+++ my-react-app/src/Scoreboard.jsx
@@ -3,8 +3,8 @@ import "./Scoreboard.css"
 const Scoreboard = (props) => {
     return (
         <div className="scoreboard">
-            <div>Current Score: {props.currentScore}</div>
-            <div>Best Score: {props.bestScore}</div>
+            <div>Current Score: <b>{props.currentScore}</b></div>
+            <div>Best Score: <b>{props.bestScore}</b></div>
         </div>
     );
 }
